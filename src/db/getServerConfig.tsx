e40dea128c1545d6c3bc5885af5300f8cfaa9dd8
alandas71 <alanalves.dev@gmail.com>
2025-05-26 09:19:59 -0300

    Delete pastas de api

--- src/db/getServerConfig.tsx
+++ src/db/getServerConfig.tsx
@@ -8,10 +8,10 @@ interface connProps {
 export function getServerConfig() {
 
   const config: connProps = {
-      DataSource: `${process.env.DB_HOST},${process.env.DB_PORT}`,
-      Catalog: process.env.DB_NAME || "",
-      Uid: process.env.DB_USER || "",
-      Pwd: process.env.DB_PSWD || "",
+    DataSource: `${process.env.DB_HOST},${process.env.DB_PORT}`,
+    Catalog: process.env.DB_NAME || "",
+    Uid: process.env.DB_USER || "",
+    Pwd: process.env.DB_PSWD || "",
   };
   
   return config;
