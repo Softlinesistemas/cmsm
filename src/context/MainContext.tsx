b3b36fbcc023b8ab7bb4890e12d16b8e8f399f5d
Softlinesistemas <155544674+Softlinesistemas@users.noreply.github.com>
2025-07-07 16:56:21 -0300

    Update MainContext.tsx

--- src/context/MainContext.tsx
+++ src/context/MainContext.tsx
@@ -1,4 +1,4 @@
-"use client" 
+"use client"
 import React, { createContext, useState, useEffect, useMemo, ReactNode } from "react";
 import Processo from '@/components/Processo'
 import Inscricoes from '@/components/Inscricoes'
