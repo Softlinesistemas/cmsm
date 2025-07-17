466f3e632c9d84fa3d13d359a673ae553763685b
Softlinesistemas <155544674+Softlinesistemas@users.noreply.github.com>
2025-07-17 16:29:10 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
